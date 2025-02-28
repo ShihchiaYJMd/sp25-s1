29224d6c88f118af9bc9d7b2dc6cac9dfdaca008
ShihchiaYJMd <ShihciaYoung@outlook.com>
2025-02-28 21:02:08 +0800

    Finished hw0a

--- hw0a/src/JavaExercises.java
+++ hw0a/src/JavaExercises.java
@@ -13,6 +13,16 @@ public class JavaExercises {
      */
     public static void starTriangle() {
         // TODO: Fill in this function
+        int numLine = 5;
+        for (int i = 1; i <= numLine; i++) {
+            for (int j = numLine - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     /**
@@ -21,6 +31,12 @@ public class JavaExercises {
      */
     public static void printIndexed(String s) {
         // TODO: Fill in this function
+        String outPut = "";
+        for (int i = 0; i < s.length(); i++){
+            outPut += s.charAt(i);
+            outPut += Integer.toString(s.length() - 1 - i);
+        }
+        System.out.println(outPut);
     }
 
     /**
@@ -29,7 +45,12 @@ public class JavaExercises {
      */
     public static String stutter(String s) {
         // TODO: Fill in this function
-        return null;
+        String outPut = "";
+        for (int i = 0; i < s.length(); i++){
+            outPut += s.charAt(i);
+            outPut += s.charAt(i);
+        }
+        return outPut;
     }
 
     /**
@@ -43,7 +64,17 @@ public class JavaExercises {
      */
     public static int quadrant(int x, int y) {
         // TODO: Fill in this function
-        return 0;
+        if (x > 0 && y > 0) {
+            return  1;
+        } else if (x < 0 && y > 0) {
+            return 2;
+        } else if (x < 0 && y < 0) {
+            return 3;
+        } else if (x > 0 && y < 0) {
+            return 4;
+        } else {
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
