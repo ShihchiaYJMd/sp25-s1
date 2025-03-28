efd30ae26fd7da7592dba1623fd9d882c7caef79
ShihchiaYJMd <ShihciaYoung@outlook.com>
2025-03-28 15:21:13 +0800

    Finished proj0

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -20,6 +20,79 @@ public class GameLogic {
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
         // TODO: Fill this in in tasks 2, 3, 4
+        /* Task 2
+        // save the original block value
+        int tileValue = board[r][c];
+        // move the tile up until the boundary or collision
+        while (r > 0 && board[r-1][c] == 0) {
+            board[r-1][c] = tileValue;
+            board[r][c] = 0;
+            r--;
+        }
+        */
+        // -------------------------------------------------------- //
+        /* Task 3: Merging Tiles
+        int tileValue = board[r][c];
+        int coeff = 0;
+        int rowNum = 0;
+        while (r > 0) {
+            if (board[r-1][c] == tileValue) {
+                tileValue *= 2;
+                board[r-1][c] = tileValue;
+                board[r][c] = 0;
+                coeff = 1;
+                rowNum = r - 1;
+                r--;
+            }
+            else if (board[r-1][c] == 0) {
+                board[r - 1][c] = tileValue;
+                board[r][c] = 0;
+                r--;
+            }
+            else {
+                break;
+            }
+        }
+        return coeff + rowNum;
+        */
+        /* Solution 3
+        int tileValue = board[r][c];
+        if (tileValue == 0) {
+            return 0;
+        }
+        while (r > 0 && board[r-1][c] == 0) {
+            board[r-1][c] = tileValue;
+            board[r][c] = 0;
+            r--;
+        }
+        if (r > 0 && board[r-1][c] == tileValue) {
+            board[r-1][c] *= 2;
+            board[r][c] = 0;
+            return r;
+        }
+        return 0;
+        */
+        // -------------------------------------------------------- //
+        /* Task 4: Merging Tiles up to MinR */
+        int tileValue = board[r][c];
+        if (tileValue == 0) {
+            return 0;
+        }
+        while (r > minR) {
+            // merge happens only once and jumps out of the loop
+            if (board[r - 1][c] == tileValue) {
+                tileValue *= 2;
+                board[r - 1][c] = tileValue;
+                board[r][c] = 0;
+                return r;
+            }
+            if (board[r - 1][c] != 0) {
+                break;
+            }
+
+            board[r - 1][c] = tileValue;
+            board[r--][c] = 0;
+        }
         return 0;
     }
 
@@ -32,7 +105,15 @@ public class GameLogic {
      */
     public static void tiltColumn(int[][] board, int c) {
         // TODO: fill this in in task 5
-        return;
+        int minR = 0;
+        for (int r = 1; r < board.length; r++) {
+            if (board[r][c] != 0) {
+                int result = moveTileUpAsFarAsPossible(board, r, c, minR);
+                if (result > 0) {
+                    minR = result;
+                }
+            }
+        }
     }
 
     /**
@@ -42,7 +123,9 @@ public class GameLogic {
      */
     public static void tiltUp(int[][] board) {
         // TODO: fill this in in task 6
-        return;
+        for (int col = 0; col < board.length; col++) {
+            tiltColumn(board, col);
+        }
     }
 
     /**
@@ -55,13 +138,21 @@ public class GameLogic {
     public static void tilt(int[][] board, Side side) {
         // TODO: fill this in in task 7
         if (side == Side.EAST) {
-            return;
+            rotateLeft(board);
+            tiltUp(board);
+            rotateRight(board);
         } else if (side == Side.WEST) {
-            return;
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
         } else if (side == Side.SOUTH) {
-            return;
+            rotateRight(board);
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
+            rotateLeft(board);
         } else {
-            return;
+            tiltUp(board);
         }
     }
 }
